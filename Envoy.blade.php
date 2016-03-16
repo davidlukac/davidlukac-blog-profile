9d4aa16778c7210326542b7327218426f62edf94
David Lukac <david.lukac@gmail.com>
2016-03-16 15:54:49 +0100

    Issue #5 - Edd initial `Envoy` setup [broken build!].

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,23 +1,62 @@
+{{-- === SERVERS ========================================================== --}}
+
 @servers(['ws' => 'uid111384@shellserver.websupport.sk -p 15709'])
 
-@task('ping', ['on' => 'ws'])
-echo "Hello world"
+{{-- === SETUP ============================================================ --}}
+
+@setup
+    $secret_folder = "~/davidlukac.com/secret";
+@endsetup
+
+{{-- === MACROS =========================================================== --}}
+
+@macro('deploy-dev')
+    fetch-secret
+    deploy-dev
+@endmacro
+
+{{-- === TASKS ============================================================ --}}
+
+@task('ping-ws', ['on' => 'ws'])
+    echo "Hello world from Websupport server!"
+    echo $(pwd)
 @endtask
 
 @task('deploy-dev', ['on' => 'ws'])
-cd davidlukac.com/sub/dev/
-git checkout develop
-git pull
-drush @dev.davidlukac.com status
+    cd davidlukac.com/sub/dev/
+    git checkout develop
+    git pull
+    drush @dev.davidlukac.com status
 @endtask
 
 @task('drush-check', ['on' => 'ws'])
-HOME_START=$(pwd)
-cd davidlukac.com/sub/dev/
-echo "Drush commands in DEV:"
-drush sa
-cd ${HOME_START}
-cd davidlukac.com/web
-echo "Drush commands in PROD:"
-drush sa
+    echo "Running Drush availability check."
+    echo "Starting folder is ${INIT_DIR}."
+    eval "pwd"
+
+    cd davidlukac.com/sub/dev/
+    echo "Drush commands in DEV:"
+    drush sa
+    echo '================================================================================'
+    {{--drush @dl.dev status--}}
+
+    cd ${INIT_DIR}
+    cd davidlukac.com/web
+    echo "Drush commands in PROD:"
+    drush sa
+    echo '================================================================================'
+    local CMD="drush @dl.prod status"
+    echo "Running: '${CMD}'."
+    eval ${CMD}
+    {{--drush @dl.prod status--}}
+@endtask
+
+@task('temp', ['on' => 'ws'])
+@endtask
+
+@task('fetch-secret', ['on' => 'ws'])
+    cd {{ $secret_folder }}
+    echo "Fetching secret stuff in: "
+    git checkout master
+    git pull
 @endtask
