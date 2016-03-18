744a8644d3ad097bf83c0c4c2bd6a766934a295d
David Lukac <david.lukac@gmail.com>
2016-03-18 21:18:12 +0100

    Issue #5 - fix local settings.

--- profiles/davids_blog/config/local/settings.local.php
+++ profiles/davids_blog/config/local/settings.local.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * @file
- * Local settings.php.
+ * Local settings.LOCAL.php.
  */
 
 // Specify where's the secure folder located.
 // Local location of secure folder.
-define('CUSTOM_CONFIG_SECURE_ROOT', DRUPAL_ROOT . '/.secure/');
+define('CUSTOM_CONFIG_SECURE_ROOT', DRUPAL_ROOT . '/secret/');
 
 // Setting Base URL - NO trailing slash!
-$base_url = 'http://davidlukac.dd';
+$base_url = 'http://davidlukac.dd:8083';
 
 // Setting cookie domain.
 // $cookie_domain = '.davidlukac.com';
