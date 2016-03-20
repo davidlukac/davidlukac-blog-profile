47a110c782e2c7a013d1de04f6ce144181986bd0
David Lukac <david.lukac@gmail.com>
2016-03-20 19:47:04 +0100

    Issue #28 - local memcache settings.

--- profiles/davids_blog/config/local/settings.local.php
+++ profiles/davids_blog/config/local/settings.local.php
@@ -32,3 +32,38 @@ $conf['page_cache_maximum_age'] = 0;
 $conf['page_compression'] = 0;
 $conf['preprocess_css'] = 0;
 $conf['preprocess_js'] = 0;
+
+/**
+ * Memcache settings.
+// */
+$memcache_storage_path = 'profiles/davids_blog/modules/contrib/memcache_storage/';
+// Move all cached data (except form cache) to memcache storage.
+$conf['cache_backends'][] = $memcache_storage_path . 'memcache_storage.inc';
+$conf['cache_default_class'] = 'MemcacheStorage';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['cache_class_cache_update'] = 'DrupalDatabaseCache';
+
+// Advanced usage of Drupal page cache.
+$conf['cache_backends'][] = $memcache_storage_path . 'memcache_storage.page_cache.inc';
+$conf['cache_class_cache_page'] = 'MemcacheStoragePageCache';
+
+// Do not connect to the database when serving cached page for anonymous users.
+$conf['page_cache_invoke_hooks'] = FALSE;
+$conf['page_cache_without_database'] = TRUE;
+
+// Open persistent memcached connection.
+$conf['memcache_storage_persistent_connection'] = TRUE;
+
+// Set unique prefix for our site.
+$conf['memcache_storage_key_prefix'] = 'davidllukac-com-local';
+
+// Move storage for lock system into memcached.
+$conf['lock_inc'] = $memcache_storage_path . 'includes/lock.inc';
+
+// Move storage for sessions into memcached.
+$conf['session_inc'] = $memcache_storage_path . 'includes/session.inc';
+
+// Set correct memcache server.
+$conf['memcache_servers'] = array(
+  'memcache_srv:32768' => 'default',
+);
